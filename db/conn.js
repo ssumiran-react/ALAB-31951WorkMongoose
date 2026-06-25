01e5cadd9b642f7d28f33b0f7436f2b45f01776f
ssumiran <ssumiran@yahoo.com>
2026-06-25 19:20:10 -0400

    Add db connection

--- db/conn.js
+++ db/conn.js
@@ -1,13 +1,12 @@
 //import { MongoClient } from "mongodb";
 import mongoose from "mongoose";
 
-
 async function connDB() {
   //let conn;
   try {
     await mongoose.connect(process.env.ATLAS_URI);     //MongoClient(process.env.ATLAS_URI);
     //conn = client;
-    console.log('Connected to MongoDB')
+    console.log('Conn MongoDB on mongoose')
   } catch (e) {
     console.error(e);
   }
@@ -15,3 +14,4 @@ async function connDB() {
   //let db = conn.db("sample_training");
 }
 export default connDB;
+
